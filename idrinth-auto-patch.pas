73e8387a02f530efe84d0e5394d61b928cd0f17b
Björn Büttner <Idrinth@users.noreply.github.com>
2021-09-02 22:30:52 +0200

    Update idrinth-auto-patch.pas

--- idrinth-auto-patch.pas
+++ idrinth-auto-patch.pas
@@ -1,8 +1,5 @@
 unit userscript;
 
-const
-  iESLMaxRecords = $800; // max possible new records in ESL
-  iESLMaxFormID = $fff; // max allowed FormID number in ESL
 var
   f: IwbFile;
   signatures, flags, lists, blacklist, blockcopy: TStringList;
@@ -598,59 +595,6 @@ begin
   end;
   if cleanOften then
     CleanMasters(f);
-end;
-
-
-function isESLable(): integer;
-var
-  i: Integer;
-  e: IInterface;
-  RecCount, RecMaxFormID, fid: Cardinal;
-  HasCELL: Boolean;
-begin
-  // iterate over all records in plugin
-  for i := 0 to Pred(RecordCount(f)) do begin
-    e := RecordByIndex(f, i);
-    
-    // override doesn't affect ESL
-    if not IsMaster(e) then
-      Continue;
-    
-    if Signature(e) = 'CELL' then
-      HasCell := True;
-    
-    // increase the number of new records found
-    Inc(RecCount);
-    
-    // no need to check for more if we are already above the limit
-    if RecCount > iESLMaxRecords then
-    begin
-      Result := 0;
-      Exit;
-    end;
-    
-    // get raw FormID number
-    fid := FormID(e) and $FFFFFF;
-    
-    // determine the max one
-    if fid > RecMaxFormID then
-      RecMaxFormID := fid;
-  end;
-
-  // too many new records, can't be ESL
-  if RecCount > iESLMaxRecords then
-    Exit;
-  
-  AddMessage(Name(f));
-  
-  if RecMaxFormID <= iESLMaxFormID then
-    AddMessage(#9'Can be turned into ESL by adding ESL flag in TES4 header')
-  else
-    AddMessage(#9'Can be turned into ESL by compacting FormIDs first, then adding ESL flag in TES4 header');
-    
-  // check if plugin has new cell(s)
-  if HasCELL then
-    AddMessage(#9'Warning: Plugin has new CELL(s) which won''t work when turned into ESL and overridden by other mods due to the game bug');
 end;
 
 function Finalize: integer;
