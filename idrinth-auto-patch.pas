f3afdaee43fb08f6981b747a95a49dfc05def120
Björn Büttner <Idrinth@users.noreply.github.com>
2021-09-02 22:29:36 +0200

    Update idrinth-auto-patch.pas

--- idrinth-auto-patch.pas
+++ idrinth-auto-patch.pas
@@ -1,5 +1,8 @@
 unit userscript;
 
+const
+  iESLMaxRecords = $800; // max possible new records in ESL
+  iESLMaxFormID = $fff; // max allowed FormID number in ESL
 var
   f: IwbFile;
   signatures, flags, lists, blacklist, blockcopy: TStringList;
@@ -175,11 +178,7 @@ begin
   for i := 0 to ElementCount(e)-1 do
   begin
     element := ElementByIndex(e, i);
-    name := BaseName(element);
-    if name = '' then
-      name := Signature(element);
-    if name = '' then
-      name := Name(element);
+    name := Name(element);
     if name = '' then
       Continue;
     if name = 'Record Header' then
@@ -599,6 +598,59 @@ begin
   end;
   if cleanOften then
     CleanMasters(f);
+end;
+
+
+function isESLable(): integer;
+var
+  i: Integer;
+  e: IInterface;
+  RecCount, RecMaxFormID, fid: Cardinal;
+  HasCELL: Boolean;
+begin
+  // iterate over all records in plugin
+  for i := 0 to Pred(RecordCount(f)) do begin
+    e := RecordByIndex(f, i);
+    
+    // override doesn't affect ESL
+    if not IsMaster(e) then
+      Continue;
+    
+    if Signature(e) = 'CELL' then
+      HasCell := True;
+    
+    // increase the number of new records found
+    Inc(RecCount);
+    
+    // no need to check for more if we are already above the limit
+    if RecCount > iESLMaxRecords then
+    begin
+      Result := 0;
+      Exit;
+    end;
+    
+    // get raw FormID number
+    fid := FormID(e) and $FFFFFF;
+    
+    // determine the max one
+    if fid > RecMaxFormID then
+      RecMaxFormID := fid;
+  end;
+
+  // too many new records, can't be ESL
+  if RecCount > iESLMaxRecords then
+    Exit;
+  
+  AddMessage(Name(f));
+  
+  if RecMaxFormID <= iESLMaxFormID then
+    AddMessage(#9'Can be turned into ESL by adding ESL flag in TES4 header')
+  else
+    AddMessage(#9'Can be turned into ESL by compacting FormIDs first, then adding ESL flag in TES4 header');
+    
+  // check if plugin has new cell(s)
+  if HasCELL then
+    AddMessage(#9'Warning: Plugin has new CELL(s) which won''t work when turned into ESL and overridden by other mods due to the game bug');
 end;
 
 function Finalize: integer;
