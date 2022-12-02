99ac3bdc764999a9bd4fdff1d045c1a04ff1012a
Björn Büttner <Idrinth@users.noreply.github.com>
2022-12-02 15:01:51 +0100

    Update idrinth-auto-patch.pas

--- idrinth-auto-patch.pas
+++ idrinth-auto-patch.pas
@@ -678,10 +678,10 @@ begin
   begin
     e1 := ElementByPath(one, paths[i]);
     e2 := ElementByPath(two, paths[i]);
+    if not Exists(e1) or not Exists (e2) then
+      Exit
     if IsInList(flags, e1, one) then
     begin
-      AddMessage(GetTypeKind(GetNativeValue(e1)));
-      AddMessage(GetTypeKind(GetNativeValue(e2)));
       if GetNativeValue(e1) <> GetNativeValue(e2) then
         Exit;
       Continue;
